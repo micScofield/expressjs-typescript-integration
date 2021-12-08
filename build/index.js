f3acda91d5b02800a7e57fadb260d1c8c46e9e61
Sanyam Jain <sanyamjaino98@gmail.com>
2021-12-09 01:25:28 +0530

    First Run

--- build/index.js
+++ build/index.js
@@ -1,2 +1,11 @@
 "use strict";
-console.log("Hi there");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
+var app = (0, express_1.default)();
+app.get('/', function (req, res) {
+    res.send("\n        <h1>Hello World</h1>\n    ");
+});
+app.listen(3000, function () { return console.log("Listening on port 3000"); });
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("Hi there");
+import express, { Request, Response } from 'express';
+
+const app = express();
+
+app.get('/', (req: Request, res: Response) => {
+    res.send(`
+        <h1>Hello World</h1>
+    `)
+})
+
+app.listen(3000, () => console.log(`Listening on port 3000`));
